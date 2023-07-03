e25f1dba6bdd2f2b4f0b536e9e298fbb612318d2
matyo82 <matingarshasbii@gmail.com>
2023-07-04 02:55:22 +0330

    sho ferst model in page html!

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    titel = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField()
--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
+def home(request):
+    person = {'name':'matin'}
+    all = Todo.objects.all()
+    return render(request, 'index.html', {'all':all})
