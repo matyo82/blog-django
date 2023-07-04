40e7111d1caadabe2568f5bb0c6b071a61d7f962
matyo82 <matingarshasbii@gmail.com>
2023-07-04 09:42:18 +0330

    add a them and add home page and shwo post

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('post/<int:id_post>/',views.post,name='post')
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ def home(request):
     person = {'name':'matin'}
     all = Todo.objects.all()
     return render(request, 'index.html', {'all':all})
+def post(request, id_post):
+    post = Todo.objects.get(id=id_post)
+    return render(request, 'post.html', {'post':post})
