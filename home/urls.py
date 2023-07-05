1e7324fa79269ee31a136f9a9f008083acbc8fdc
matyo82 <matingarshasbii@gmail.com>
2023-07-06 01:34:51 +0330

    add btn for delete and add masseges for delete post

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home),
-    path('post/<int:id_post>/',views.post,name='post')
-]
+    path('',views.home,name='home'),
+    path('post/<int:id_post>/',views.post,name='post'),
+    path('delete/<int:Todo_id>/',views.delete,name='delete'),
+] 
--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
-    person = {'name':'matin'}
     all = Todo.objects.all()
     return render(request, 'index.html', {'all':all})
 def post(request, id_post):
     post = Todo.objects.get(id=id_post)
     return render(request, 'post.html', {'post':post})
+
+def delete(request, Todo_id):
+    Todo.objects.get(id=Todo_id).delete()
+    messages.success(request, 'delete the post success!!', 'success')
+    return redirect('home')
